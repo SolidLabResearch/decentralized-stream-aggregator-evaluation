3717b90900d808309e7281148c1fbaccc13e0cc7
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-02-07 16:09:11 +0100

    refactored the noagg-log.

--- evaluations/e-health/evaluation-1-increasing-window-sizes/without-aggregator/FileStreamer.ts
+++ evaluations/e-health/evaluation-1-increasing-window-sizes/without-aggregator/FileStreamer.ts
@@ -1,5 +1,4 @@
 import { RSPEngine, RDFStream } from "rsp-js";
-import { QueryEngine } from "@comunica/query-sparql";
 import { LDPCommunication, LDESinLDP, LDES, storeToString } from "@treecg/versionawareldesinldp";
 const N3 = require('n3');
 import fs from 'fs';
@@ -10,20 +9,20 @@ export class FileStreamer {
     private from_date: Date;
     private to_date: Date;
     public stream_name: RDFStream | undefined;
-    public comunica_engine: QueryEngine;
     public ldes!: LDESinLDP;
     public communication: LDPCommunication;
     private time_to_add_events = 0;
+    public window_width: number;
     private observation_array: any[];
 
-    constructor(ldes_stream: string, from_date: Date, to_date: Date, rspEngine: RSPEngine) {
+    constructor(ldes_stream: string, from_date: Date, to_date: Date, rspEngine: RSPEngine, window_width: number) {
         this.ldes_stream = ldes_stream;
         this.from_date = from_date;
         this.to_date = to_date;
         this.stream_name = rspEngine.getStream(ldes_stream) as RDFStream;
         this.communication = new LDPCommunication();
-        this.comunica_engine = new QueryEngine();
         this.observation_array = [];
+        this.window_width = window_width;
         this.initialize_file_streamer().then(() => {
             console.log(`Reading from the solid pod is initialized.`);
         });
@@ -32,38 +31,38 @@ export class FileStreamer {
     public async initialize_file_streamer(): Promise<void> {
         this.ldes = new LDESinLDP(this.ldes_stream, this.communication);
         let metadata = await this.ldes.readMetadata();
-        let bucket_strategy = metadata.getQuads(this.ldes_stream + "#BucketizeStrategy", "https://w3id.org/tree#path", null, null)[0].object.value;        
-        let streamer_start = Date.now();
+        let bucket_strategy = metadata.getQuads(this.ldes_stream + "#BucketizeStrategy", "https://w3id.org/tree#path", null, null)[0].object.value;
+        let reader_start = Date.now();
         const stream = await this.ldes.readMembersSorted({
             from: this.from_date,
             until: this.to_date,
             chronological: true
         });
-
+        const reader_end = Date.now();
+        fs.appendFileSync(`noagg-${this.window_width}.csv`, `Reader,${(reader_end - reader_start) / 1000}s\n`);
         stream.on('data', async (data) => {
+            let pre_process_event_start = Date.now();
             let store = new N3.Store(data.quads)
             let timestamp = store.getQuads(null, bucket_strategy, null, null)[0].object.value;
             let timestamp_epoch = Date.parse(timestamp);
-            let time_start = performance.now();
-            await add_event_to_rsp_engine(store, [this.stream_name as RDFStream], timestamp_epoch);
-            let time_end = performance.now();
-            let time_taken = time_end - time_start;
-            this.time_to_add_events = this.time_to_add_events + time_taken;
-            fs.appendFileSync('events.txt', `${time_taken/1000}\n`);            
+            let pre_process_event_end = Date.now();
+            fs.appendFileSync(`noagg-${this.window_width}.csv`, `pre_process,${(pre_process_event_end - pre_process_event_start)}ms\n`);
+            let time_start = Date.now();
+            await add_event_to_rsp_engine(store, [this.stream_name as RDFStream], timestamp_epoch).then(() => {
+                let time_end = Date.now();
+                fs.appendFileSync(`noagg-${this.window_width}.csv`, `add_event,${(time_end - time_start)}ms\n`);
+            });
         });
         stream.on('end', async () => {
-            let streamer_end = Date.now();
-            console.log(`Time to add events to the RSP-Engine: ${this.time_to_add_events / 1000}s`);
-            fs.appendFileSync('streamer.txt', `${(streamer_end - streamer_start) / 1000}s\n`);
             console.log(`Decentralized File Streamer has ended.`);
         });
     }
 }
 
 export async function add_event_to_rsp_engine(store: any, stream_name: RDFStream[], timestamp: number) {
-    stream_name.forEach(async(stream: RDFStream) => {
+    stream_name.forEach(async (stream: RDFStream) => {
         let quads = store.getQuads(null, null, null, null);
-        for (let quad of quads) {            
+        for (let quad of quads) {
             stream.add(quad, timestamp);
         }
     });
--- evaluations/e-health/evaluation-1-increasing-window-sizes/without-aggregator/StreamProcessor.ts
+++ evaluations/e-health/evaluation-1-increasing-window-sizes/without-aggregator/StreamProcessor.ts
@@ -1,6 +1,7 @@
 import { RDFStream, RSPQLParser, RSPEngine } from 'rsp-js';
 import { FileStreamer } from './FileStreamer';
 import { EventEmitter } from 'events';
+import fs from 'fs';
 
 export class StreamProcessor {
     public query: string;
@@ -8,6 +9,9 @@ export class StreamProcessor {
     public to_date: Date;
     public stream_array: string[];
     public parser: RSPQLParser;
+    public streamer_start_time: number = 0;
+    public streamer_end_time: number|null = null;
+    public window_width: number;
     public rsp_engine: RSPEngine;
     public rsp_emitter: EventEmitter;
 
@@ -21,14 +25,16 @@ export class StreamProcessor {
         this.parser.parse(this.query).s2r.forEach((stream) => {
             this.stream_array.push(stream.stream_name);
         });
+        this.window_width = this.parser.parse(this.query).s2r[0].width;
         this.rsp_emitter = this.rsp_engine.register();
         this.start_streamer();
     }
 
     public async start_streamer() {
         for (const stream of this.stream_array) {
+            this.streamer_start_time = Date.now();
             console.log(`Starting the FileStreamer for ${stream}`);
-            new FileStreamer(stream, this.from_date, this.to_date, this.rsp_engine);
+            new FileStreamer(stream, this.from_date, this.to_date, this.rsp_engine, this.window_width);
         }
         this.execute_stream_processor();
     }
@@ -36,6 +42,10 @@ export class StreamProcessor {
     public async execute_stream_processor() {
         console.log(`Waiting for events from the RStream.`);
         this.rsp_emitter.on('RStream', async (object: any) => {
+            if (this.streamer_end_time === null) {
+                this.streamer_end_time = Date.now();
+                fs.appendFileSync(`noagg-${this.window_width}.csv`, `${this.window_width},${(this.streamer_end_time - this.streamer_start_time) / 1000}s\n\n`);
+            }
             let iterable = object.bindings.values();
             for (let item of iterable) {
                 if (item.value) {
